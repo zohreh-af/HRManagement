f7b30d3cb26a99e1740f269335b41de4edbe95d7
zohreh <abbasifar.zohreh@gmail.com>
2025-09-21 01:45:16 +0330

    14040629-add IsUthenticated, change main layout#8
    Related Work Items: #8

--- BlazorHRManagement.Web/Components/Layout/MainLayout.razor.cs
+++ BlazorHRManagement.Web/Components/Layout/MainLayout.razor.cs
@@ -1,4 +1,5 @@
-﻿using HRManagement.Infrastructure.Web.Services;
+﻿using HRManagement.Identity.Client.Services;
+using HRManagement.Infrastructure.Web.Services;
 using HRManagement.Presentation;
 using Microsoft.AspNetCore.Components;
 
@@ -7,12 +8,25 @@ namespace BlazorHRManagement.Web.Components.Layout;
 public partial class MainLayout
 {
     private string currentTitle = string.Empty;
+    public bool IsAuthenticated = false;
     [Inject] public AppTitleState TitleState { get; set; }
-
+    [Inject] public AppAuthenticationStateProvider AuthState { get; set; }
     protected override async Task OnInitializedAsync()
     {
         currentTitle = TitleState.Title;
         TitleState.Changed += OnTitleChanged;
+
+        var state = await AuthState.GetAuthenticationStateAsync();
+        if (state.User.Identity.IsAuthenticated)
+        {
+            IsAuthenticated =true;
+        }
+    }
+    private void AuthState_AuthenticationStateChanged(Task<Microsoft.AspNetCore.Components.Authorization.AuthenticationState> task)
+    {
+        IsAuthenticated = task.GetAwaiter().GetResult().User.Identity.IsAuthenticated;
+
+        StateHasChanged();
     }
 
     private void OnTitleChanged()
